7882cee3e31fd34a2331a041c8ad82cd461fcc0c
Losen2133 <127107157+Losen2133@users.noreply.github.com>
2025-07-03 17:08:03 +0800

    Add review routes to API
    Added POST, PUT, and DELETE routes for reviews in the API, handled by ReviewController. This enables creating, updating, and deleting reviews via the API.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\GenreController;
+use App\Http\Controllers\Api\ReviewController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,5 +15,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('authors', AuthorController::class);
     Route::apiResource('books', BookController::class);
     Route::apiResource('genres', GenreController::class);
+    Route::post('reviews', [ReviewController::class, 'store']);
+    Route::put('reviews/{review}', [ReviewController::class, 'update']);
+    Route::delete('reviews/{review}', [ReviewController::class,'delete']);
 });
 
